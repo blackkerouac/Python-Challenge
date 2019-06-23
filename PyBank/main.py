d06748851eca14e2010126679f1b5c9e187a88c0
blackkerouac <50513253+blackkerouac@users.noreply.github.com>
2019-06-23 08:37:36 -0700

    second commit

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,23 +3,27 @@
 
 import csv
 
-
-
 budgetfile =  "./budget-data.csv"
+
+total_months = 0
+net_prof = 0
+changes = 0
+total_revenue = 0
+months_count = 0
+last_month = 0
+great_inc = 0
+great_los = 0
+inc_month = ""
+los_month = ""
+rev_change = []
 months = []
 prof_loss=[]
 current_month = []
-last_month = 0
 change =[]
 with open(budgetfile, "r") as bank_data:
     reader = csv.reader(bank_data,)
     next(reader)
-    total_months = 0
-    net_prof = 0
-    changes = 0
-    rev_change = []
-    total_revenue = 0
-    months_count = 0
+   
     for row in reader:
         #total months
         total_months = total_months + 1
@@ -31,20 +35,29 @@ with open(budgetfile, "r") as bank_data:
         #change in revenue
         current_month = int(row[1])
         total_revenue = total_revenue + current_month
-        if months_count > 1:
+        if months_count > 0:
             changes = current_month - last_month
             rev_change.append(changes)
+            if changes > great_inc:
+                great_inc = changes
+                inc_month = row[0]
+            elif changes < great_los:
+                great_los = changes
+                los_month = row[0]
+        else:
+            months_count += 1 
         last_month = current_month
-
+        
 sum_rev_changes = sum(rev_change)
 avg_change = sum_rev_changes / (total_months - 1)
 
-
+print(sum(rev_change) / len(rev_change))
 print(f"Financial Analysis")
 print("----------------------------------------")
 print("                                        ")
 print(f'Total Months: {total_months}')
 print(f'Total: ${net_prof}')
 print(f"Average Revenue Change: ${avg_change}")
-
+print(los_month)
+print (inc_month)
 #this is as far I got to meet the deadline :(
